7c8b00f672571573e1e7196e2729019727049f0e
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-20 11:46:52 +0100

    Added .csv files

--- src/main/java/Buildweek2/enteties/User.java
+++ src/main/java/Buildweek2/enteties/User.java
@@ -1,14 +1,19 @@
 package Buildweek2.enteties;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.UUID;
+
 @Getter
 public class User implements UserDetails {
-    private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
