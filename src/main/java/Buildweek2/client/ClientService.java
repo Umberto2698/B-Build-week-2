2f58416a0e6dc5c5565ea234a891047c37b7249f
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-20 16:04:19 +0100

    change notFoundException

--- src/main/java/Buildweek2/client/ClientService.java
+++ src/main/java/Buildweek2/client/ClientService.java
@@ -2,7 +2,7 @@ package Buildweek2.client;
 
 import Buildweek2.client.payloads.ChangeClientInfoDTO;
 import Buildweek2.client.payloads.NewClientDTO;
-import Buildweek2.exceptions.ItemNotFoundException;
+import Buildweek2.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -40,14 +40,14 @@ public class ClientService {
         return clientRepo.findAll(pageable);
     }
     public Client getSingleClient(long id){
-        return clientRepo.findById(id).orElseThrow(() -> new ItemNotFoundException(""));
+        return clientRepo.findById(id).orElseThrow(() -> new NotFoundException(id));
     }
     public void removeClient(long id){
-        Client toRemove = clientRepo.findById(id).orElseThrow(()-> new ItemNotFoundException(""));
+        Client toRemove = clientRepo.findById(id).orElseThrow(()-> new NotFoundException(id));
         clientRepo.delete(toRemove);
     }
     public Client findByIdAndUpdate(long id, ChangeClientInfoDTO body){
-        Client found = clientRepo.findById(id).orElseThrow(()-> new ItemNotFoundException(""));
+        Client found = clientRepo.findById(id).orElseThrow(()-> new NotFoundException(id));
         found.setContactName(body.contactName());
         found.setContactSurname(body.contactSurname());
         found.setContactPhone(body.contactPhone());
--- src/main/java/Buildweek2/exceptions/NotFoundException.java
+++ src/main/java/Buildweek2/exceptions/NotFoundException.java
@@ -2,7 +2,7 @@ package Buildweek2.exceptions;
 
 public class NotFoundException extends RuntimeException {
   public NotFoundException(Long id) {
-    super("There is no item with this id");
+    super("There is no item with this id: " + id);
   }
 
   public NotFoundException(String message) {
