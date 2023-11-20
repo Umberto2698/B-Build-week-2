37c9ef4a2c2a5580199fc5e35470953ac1ceeece
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-20 15:51:46 +0100

    Removed annotations

--- src/main/java/Buildweek2/user/payloads/RoleUpdateDTO.java
+++ src/main/java/Buildweek2/user/payloads/RoleUpdateDTO.java
@@ -2,10 +2,9 @@ package Buildweek2.user.payloads;
 
 import Buildweek2.user.UserRole;
 import Buildweek2.user.validator.ValidRole;
-import jakarta.validation.constraints.NotNull;
 
-public record RoleUpdateDTO(@NotNull(message = "The role is required.")
-                            @ValidRole(enumClass = UserRole.class, message = "The type of device must be one of these:" +
-                                    "USER, ADMIN")
-                            UserRole role) {
+public record RoleUpdateDTO(
+        @ValidRole(enumClass = UserRole.class, message = "The type of device must be one of these:" +
+                "USER, ADMIN")
+        UserRole role) {
 }
--- src/main/java/Buildweek2/user/payloads/UserDTO.java
+++ src/main/java/Buildweek2/user/payloads/UserDTO.java
@@ -3,7 +3,6 @@ package Buildweek2.user.payloads;
 import Buildweek2.user.UserRole;
 import Buildweek2.user.validator.ValidRole;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record UserDTO(@NotEmpty(message = "The name is required.")
@@ -16,7 +15,6 @@ public record UserDTO(@NotEmpty(message = "The name is required.")
                       @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Insert a valid email.")
                       String email,
                       String username,
-                      @NotNull(message = "The role is required.")
                       @ValidRole(enumClass = UserRole.class, message = "The type of device must be one of these:" +
                               "USER, ADMIN")
                       UserRole role
