3775812e0d9f8e0b2616dd1903f53ed62231e3bb
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-20 21:26:29 +0100

    Added class

--- src/main/java/Buildweek2/bill/Payloads/BillDTO.java
+++ src/main/java/Buildweek2/bill/Payloads/BillDTO.java
@@ -7,12 +7,13 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 
-public record BillDTO(@NotNull(message = "Amount field is required!") @Min(value=1, message = "Value amount must be greater than or equal to 1 ") long amount,
-                      @NotEmpty(message = "Client Id Field Required!") Long  clientId,
-                      @NotEmpty(message = "User Id Field Required!") Long userId,
-                      @ValidBillState(enumClass = BillState.class,
-                      message = "The Bill can be:" +
-                              "  PAID or UNPAID") BillState billState
+public record BillDTO(
+        @NotNull(message = "Amount field is required!") @Min(value = 1, message = "Value amount must be greater than or equal to 1 ") long amount,
+        @NotEmpty(message = "Client Id Field Required!") Long clientId,
+        @NotEmpty(message = "User Id Field Required!") Long userId,
+        @ValidBillState(enumClass = BillState.class,
+                message = "The Bill can be:" +
+                        "  PAID or UNPAID") BillState billState
 
 ) {
 }
