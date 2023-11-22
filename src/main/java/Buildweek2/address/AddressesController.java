1dddb7b5e9c4702ab157b0232b28042b71b851bb
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-22 11:55:15 +0100

    Update AddressesController.java

--- src/main/java/Buildweek2/address/AddressesController.java
+++ src/main/java/Buildweek2/address/AddressesController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/adress")
+@RequestMapping("/addresses")
 public class AddressesController {
     @Autowired
     AddressesService addressesService;
