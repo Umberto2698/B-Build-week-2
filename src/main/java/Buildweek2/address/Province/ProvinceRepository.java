768d4fd97fbbcafed629ee60d9e2d6382939d607
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-23 17:12:58 +0100

    added files

---
+++ src/main/java/Buildweek2/address/Province/ProvinceRepository.java
@@ -0,0 +1,9 @@
+package Buildweek2.address.Province;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinceRepository extends JpaRepository<Province, Long> {
+    Province findByProvinceName(String s);
+}
