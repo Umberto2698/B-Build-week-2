1921622105ee765ab4f693d46eae6301e1d3a4bc
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-23 17:10:19 +0100

    bug

--- src/main/java/Buildweek2/address/Province/ProvinceRepository.java
+++
@@ -1,9 +0,0 @@
-package Buildweek2.address.Province;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProvinceRepository extends JpaRepository<Province, Long> {
-    Province findByProvinceName(String s);
-}
